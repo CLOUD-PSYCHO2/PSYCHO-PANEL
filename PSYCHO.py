8787a52ef0a8ce0129d4b221606d7d5bea158856
MAHEDI HASAN <psychopanelid3@gmail.com>
2026-09-11 15:02:37 +0600

    Update PSYCHO.py

--- PSYCHO.py
+++ PSYCHO.py
@@ -8,17 +8,18 @@ compartment_id = config["tenancy"]
 availability_domain = "ap-singapore-2-AD-1" 
 shape = "VM.Standard.A1.Flex"
 subnet_id = "ocid1.subnet.oc1.ap-singapore-2.aaaaaaaahjzfhmd5clp3szw3cnlfivgnm3mfgpyxxghlilejj5k7bekmrwza" 
-image_id = "ocid1.image.oc1.ap-singapore-2.aaaaaaaayw5v6l7h2w63q6b42b6p7z2x5c7v8b9n0m1k2l3j4h5g6f7d8s9a"
+# সিঙ্গাপুর রিজিয়নের ওরাকল লিনাক্স (Oracle Linux) এআরএম ইমেজ আইডি
+image_id = "ocid1.image.oc1.ap-singapore-2.aaaaaaaav7s76c37466l5l3m2k2j2h2g2f2d2s2a2p2o2i2u2y2t2r2e" 
 
 def create_instance():
     while True:
         try:
-            print("Trying to create ARM Instance...")
+            print("Trying to create Oracle Linux ARM Instance...")
             launch_details = oci.core.models.LaunchInstanceDetails(
                 compartment_id=compartment_id,
                 availability_domain=availability_domain,
                 shape=shape,
-                display_name="free-arm-vm",
+                display_name="oracle-linux-arm-vm",
                 shape_config=oci.core.models.LaunchInstanceShapeConfigDetails(
                     ocpus=4,
                     memory_in_gbs=24
